b45ec60415b9bf8129118868f1b6e29aeb391150
gizemvc <antrali07gizemo@gmail.com>
2024-12-12 23:32:13 +0300

    Add files via upload

---
+++ OtoServisSatis.Data/DatabaseContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using OtoServisSatis.Entities;
+
+
+namespace OtoServisSatis.Data
+{
+    public class DatabaseContext :DbContext
+
+    {
+        public DbSet<Arac> Araclar { get; set; }
+        public DbSet<Kullanici> Kullanicilar { get; set; }          
+        public DbSet<Marka> Markalar { get; set; }
+        public DbSet<Musteri> Musteriler { get; set; }  
+        public DbSet<Rol> Roller { get; set; }
+        public DbSet<Satis> Satislar { get; set; }
+        public DbSet<Servis> Servisler { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=DESKTOP-ML8F0M1; database=OtoServisSatis; TrustServerCertificate=True;
+            integrated security=True; MultipleActiveResultSets=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Fluent API
+            modelBuilder.Entity<Marka>().Property(m=>m.Adi).IsRequired().HasColumnType("varchar(50)");
+            modelBuilder.Entity<Rol>().Property(m=>m.Adi).IsRequired().HasColumnType("varchar(50)");
+            modelBuilder.Entity<Rol>().HasData(new Rol
+            {
+                Id = 1,
+                Adi = "Admin"
+            });
+
+            modelBuilder.Entity<Kullanici>().HasData(new Kullanici
+            {
+               Id = 1,
+               Adi = "Admin",
+               AktifMi = true,
+               EklenmeTarihi = DateTime.Now,
+               Email = "admin@otoservissatiis.tc",
+               KullaniciAdi = "admin",
+               Sifre = "123456",
+               //Rol = new Rol { Id = 1 },
+               RolId = 1,
+               SoyAdi = "admin",
+               Telefon = "0850",
+
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ OtoServisSatis.Entities/Arac.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OtoServisSatis.Entities
+{
+    public class Arac : IEntity
+    {
+        public int Id { get; set; } 
+        public int MarkaId { get; set; }
+        [StringLength(50)]
+        public string Renk { get; set; }  
+        public decimal Fiyati { get; set; }
+        [StringLength(50)]
+        public string Modeli { get; set; }
+        public string MasaTipi { get; set; }    
+        public int  ModelYili { get; set; } 
+        public bool SatistaMi { get; set; } 
+        public string Notlar {  get; set; }
+        public virtual Marka? Marka { get; set; } //Araç sınıfı ile Marka sınıfı arasında bağlantı
+    }
+}
